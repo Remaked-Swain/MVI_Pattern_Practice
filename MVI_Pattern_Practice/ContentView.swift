0c2fabe548723c38739d21b053e10da59b8e77ef
YunSwain <destap@naver.com>
2024-12-23 15:30:35 +0900

    feat: First commit

---
+++ MVI_Pattern_Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVI_Pattern_Practice
+//
+//  Created by Swain Yun on 12/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVI_Pattern_Practice/MVI_Pattern_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVI_Pattern_PracticeApp.swift
+//  MVI_Pattern_Practice
+//
+//  Created by Swain Yun on 12/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MVI_Pattern_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
